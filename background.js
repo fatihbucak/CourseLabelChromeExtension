437ac3b8ea4df32b38040c2b931c8083bd2da9bf
Fatih Mustafa Bucak <fatihcm7@gmail.com>
2020-05-11 15:02:06 +0300

    Fix bugs

--- background.js
+++ background.js
@@ -1,45 +1,15 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-'use strict';
-
-chrome.runtime.onInstalled.addListener(function() {
-
-    chrome.webNavigation.onCompleted.addListener(function() {
-
-        function modifyDOM() {
-
-            var leftSide = document.getElementsByClassName("menuErisimAgaci");
-            if (leftSide.length > 0) {
-                var scripts = leftSide[0].getElementsByTagName("script");
-                var courses = leftSide[0].getElementsByTagName("span");
-                for (var i = 0; i < courses.length; i++) {
-                    if (scripts[i].innerText.includes("var title")) {
-                        var parts = scripts[i].innerText.split(";\">");
-                        var courseName = parts[1].split("</span>")[0];
-                        if (courseName.length > 25) {
-                            courseName = courseName.substring(0, 25)
-                        }
-                        courses[i].innerHTML = courseName;
-                    }
-                }
+var leftSide = document.getElementsByClassName("menuErisimAgaci");
+if (leftSide.length > 0) {
+    var scripts = leftSide[0].getElementsByTagName("script");
+    var courses = leftSide[0].getElementsByTagName("span");
+    for (var i = 0; i < courses.length; i++) {
+        if (scripts[i].innerText.includes("var title")) {
+            var parts = scripts[i].innerText.split(";\">");
+            var courseName = parts[1].split("</span>")[0];
+            if (courseName.length > 25) {
+                courseName = courseName.substring(0, 25)
             }
-
-            return document.body.innerHTML;
+            courses[i].innerHTML = courseName;
         }
-
-        chrome.tabs.executeScript({
-            code: '(' + modifyDOM + ')();' //argument here is a string but function.toString() returns function's code
-        }, (results) => {
-
-        });
-
-
-
-    }, {
-        url: [{
-            urlMatches: 'https://ninova.itu.edu.tr'
-        }]
-    });
-});
+    }
+}
